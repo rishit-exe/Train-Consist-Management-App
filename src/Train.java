5575a771d28bfabd8da475537fee91fb1dba5584
rishit-exe <rishit.vns05@gmail.com>
2026-03-24 09:17:10 +0530

    UC2: Add Passenger Bogies to Train (ArrayList Operations)

--- src/Train.java
+++ src/Train.java
@@ -15,5 +15,32 @@ public class Train {
 
         System.out.println("\nSystem ready for operations...");
 
+
+        System.out.println("===========================");
+        System.out.println("Add Passenger Bogies to Train (UC2)");
+        System.out.println("===========================");
+
+        List<String> passengerBogies = new ArrayList<>();
+
+        passengerBogies.add("Sleeper");
+        passengerBogies.add("AC Chair");
+        passengerBogies.add("First Class");
+
+        System.out.println("\nAfter adding Bogies: ");
+        System.out.println("Passenger Bogies: " + passengerBogies);
+
+        passengerBogies.remove("AC Chair");
+        System.out.println("\nAfter removing 'AC Chair': ");
+        System.out.println("Passenger Bogies: " + passengerBogies);
+
+        System.out.println("\nChecking if 'Sleeper' exists: ");
+        System.out.println("Contains Sleeper? : " + passengerBogies.contains("Sleeper"));
+
+        System.out.println("\nFinal Train Passenger Consist: ");
+        System.out.println(passengerBogies);
+
+        System.out.println("\nCRUD operations(UC2) completed successfully...");
+
+
     }
 }
